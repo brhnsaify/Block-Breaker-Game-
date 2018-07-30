da6d96f04534ce1c365a37265f9992427b2d7eef
Burhanuddin Saify <brhan@gmail.com>
2018-07-31 01:13:01 +0530

    first commit

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int MaxHits;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+
+	public AudioClip boing;
+
+	private int timesHit;
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		timesHit=0;
+		breakableCount++;
+		//print(breakableCount);
+		levelManager= GameObject.FindObjectOfType<LevelManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		//GetComponent<AudioSource>().Play();
+		AudioSource.PlayClipAtPoint(boing, transform.position);
+		timesHit++;
+		if(timesHit >= MaxHits){
+		breakableCount--;
+		levelManager.BrickDestroyed();
+		//print("dec"+breakableCount);
+		Destroy(gameObject);
+		}else{
+			LoadSprites();
+		}
+		//SimulateWin();
+	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit-1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+	}
+
+	/*//TODO test meathod remove
+	void SimulateWin(){
+		levelManager.LoadNextLevel();
+	}*/
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	//public bool autoPlay = false;
+	private Ball ball;
+	public static string action;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(action == "yes"){
+		AutoPlay();
+	}else{
+		PlayOnMouse();
+	}
+	/*
+	if(!autoPlay){
+		PlayOnMouse();
+	}else{
+		AutoPlay();
+		}*/
+	}
+
+	public void AutoPlay(){
+	Vector3 paddlepos = new Vector3(0.5f, this.transform.position.y, 0f);
+	Vector3 ballpos = ball.transform.position;
+	paddlepos.x=Mathf.Clamp(ballpos.x, 0.5f, 15.5f);
+
+	this.transform.position=paddlepos;
+	}
+
+	void PlayOnMouse(){
+
+	Vector3 paddlepos = new Vector3(0.5f, this.transform.position.y, 0f);
+
+	float mousePosInBLocks = Input.mousePosition.x/ Screen.width *16;
+
+	paddlepos.x=Mathf.Clamp(mousePosInBLocks, 0.5f, 15.5f);
+
+	this.transform.position=paddlepos;
+		
+	}
+
+	public void AutoPlayB(string action1){
+		action = action1;
+	}
+
+}
